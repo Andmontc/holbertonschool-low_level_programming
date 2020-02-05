c9bde1b11436153090a54eb14595a3f3a62f76ae
Andmontc <oscarandresmontes@gmail.com>
2020-02-05 20:14:16 +0000

    Fix the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main- Prints "Programming is like building a multilingual
+ * main - Prints "Programming is like building a multilingual
  * puzzle, followed by a new line.
  * Return:0
- **/
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle/n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
