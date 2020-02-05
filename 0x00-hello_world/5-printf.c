079e8f6ec7ba37e72d45a2b56509cbb0e2817356
Andmontc <oscarandresmontes@gmail.com>
2020-02-05 20:40:35 +0000

    Code for Hello Printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual
+ * puzzle, followed by a new line.
+ * Return:0
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
