3c00feb1b47d3fc15d2279ee39dcb11e379af56a
Andmontc <oscarandresmontes@gmail.com>
2020-02-06 20:20:44 +0000

    code for primt numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	int ab;
+
+	for (ab = 0; ab < 10; ab++)
+	{
+		printf("%d", ab);
+	}
+		putchar(10);
+
+	return (0);
+}
