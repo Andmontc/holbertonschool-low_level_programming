9a01fa508211a87041abd67d5bf87299a80576a9
Andmontc <oscarandresmontes@gmail.com>
2020-02-06 19:54:09 +0000

    code for Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	char ab;
+
+	for (ab = 'a'; ab <= 'z'; ab++)
+	{
+		putchar(ab);
+	}
+		putchar(10);
+
+	return (0);
+}
