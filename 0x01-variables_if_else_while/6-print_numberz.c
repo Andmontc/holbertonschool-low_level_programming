a76c528f4e2864d02fe73005d5626b0d4b8ccc17
Andmontc <oscarandresmontes@gmail.com>
2020-02-06 20:26:14 +0000

    code for numbers with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+	char ab;
+
+	for (ab = 48; ab <= 57; ab++)
+	{
+		putchar(ab);
+	}
+		putchar(10);
+
+	return (0);
+}
