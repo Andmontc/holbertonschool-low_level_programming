6ce9ca6b783aac6cd7fb64a78476bfdd365035da
Andmontc <oscarandresmontes@gmail.com>
2020-02-11 03:26:35 +0000

    Code for absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+* _abs - Entry point
+* @i: parameter of the function
+* Return: 1 positive -1 abs to positive
+*
+*/
+int _abs(int i)
+{
+	if (i > 0)
+	return (i);
+	else
+	return (-i);
+}
