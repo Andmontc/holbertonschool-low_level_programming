a05cb96473726c015a8922b84687d20a88f58ea8
Andmontc <oscarandresmontes@gmail.com>
2020-02-11 17:50:06 +0000

    prototypes functions

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,5 +1,5 @@
-#ifndef _SAMPLE_HEADER_H_
-#define _SAMPLE_HEADER_H_
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
 
 /*
  * Structs, enums and unions definitions
@@ -7,5 +7,17 @@
  * Function prototypes
  */
 
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
 
-#endif /* _SAMPLE_HEADER_H_ */
+#endif
