6190c0bc5c2c41e8640d9642cb53f403394429e3
Andmontc <oscarandresmontes@gmail.com>
2020-02-13 17:19:31 +0000

    code for numbers to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+* more_numbers - Entry point
+*
+*/
+void more_numbers(void)
+{
+	int contador = 0;
+	int num;
+
+		while (contador <= 10)
+		{
+			for (num = 0; num <= 14; num++)
+			{
+				int num1 = num / 10;
+				int num2 = num % 10;
+
+				if (num > 9)
+					_putchar(num1 + 48);
+					_putchar(num2 + 48);
+			}
+			_putchar('\n');
+
+			contador++;
+		}
+}
