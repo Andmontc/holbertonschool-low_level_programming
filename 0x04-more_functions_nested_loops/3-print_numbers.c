b12763e07f08652bf1ab016b9bffe09179ccfd03
Andmontc <oscarandresmontes@gmail.com>
2020-02-13 16:04:14 +0000

    code tha prints the numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* print_numbers - Entry point
+*
+*/
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + 48);
+	}
+
+	_putchar('\n');
+}
