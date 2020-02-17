4254d41931059d6c62cc77e5bd9d67adebb183ce
Andmontc <oscarandresmontes@gmail.com>
2020-02-17 23:31:42 +0000

    code that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "holberton.h"
+/**
+* _puts- function that prints a string
+* @str: variable that contains the string
+*/
+void _puts(char *str)
+{
+
+	while (*str)
+	{
+		_putchar(*str++);
+	}
+	_putchar(10);
+}
+
