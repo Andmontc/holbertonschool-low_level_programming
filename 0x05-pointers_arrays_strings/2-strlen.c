35bd468a18c638320afe30532dcbd9814443faf6
Andmontc <oscarandresmontes@gmail.com>
2020-02-17 22:56:24 +0000

    code for the lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "holberton.h"
+/**
+* _strlen - function that calculates lenght of a string
+* @str: string
+* Return: the size  of the string
+*/
+int _strlen(char *str)
+{
+	int lenght;
+
+	lenght = strlen(str);
+
+	return (lenght);
+}
+
