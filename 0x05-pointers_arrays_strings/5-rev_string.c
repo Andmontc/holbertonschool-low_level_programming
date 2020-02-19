94ab7e9ffafe08156f31cac2fddc7a37180b7b0d
Andmontc <oscarandresmontes@gmail.com>
2020-02-19 00:13:01 +0000

    code that reverse an array

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+*rev_string- function that reverse an array
+*@s: the array to reverse
+*
+*/
+void rev_string(char *s)
+{
+	int i = 0;
+	int j, tmp;
+	char fake[100];
+
+	while (s[i] != '\0')
+	{
+		fake[i] = s[i];
+		i++;
+	}
+	fake[i] = '\0';
+	tmp = i;
+
+	for (j = 0; j < tmp ; j++)
+	{
+		s[j] = fake[i - 1];
+		i--;
+	}
+}
+
