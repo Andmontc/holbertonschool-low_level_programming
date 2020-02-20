880f58f70aef29273713ee810ed1c8af9f40cf12
Andmontc <oscarandresmontes@gmail.com>
2020-02-20 16:18:43 +0000

    code that concatenate two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,24 @@
 #include "holberton.h"
 /**
-*
-*
-*
+* *_strncat - function that concatenates two strings
+* @dest: first string
+* @src: second string
+* @n: integer that counts
+* Return: the string concatenated
 */
 char *_strncat(char *dest, char *src, int n)
 {
-       int contd, conts;
+	int contd, conts;
 
-        for (contd = 0; dest[contd] != '\0'; contd++)
-        {
-        }
-        for (conts = 0; src[conts] != '\0'; conts++)
-        {
+	for (contd = 0; dest[contd] != '\0'; contd++)
+	{
+	}
+	for (conts = 0; src[conts] != '\0'; conts++)
+	{
 		if (conts < n)
 		{
-        	dest[contd + conts] = src[conts];
-        	}
+		dest[contd + conts] = src[conts];
+		}
 	}
 
 	src = '\0';
