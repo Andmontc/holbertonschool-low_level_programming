40abbae2048d4944cf1fcf4c2194576777a3aede
Andmontc <oscarandresmontes@gmail.com>
2020-02-20 23:49:23 +0000

    code tho capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+* cap_string - function that capitalize words
+* @s: string to capitalize
+* Return: string capitalized
+*
+*/
+char *cap_string(char *s)
+{
+	int i = 0, f = 0;
+
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] = s[0] - 32;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+			f++;
+		if (s[i] == ',' || s[i] == '"')
+			f++;
+		if (s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+			f++;
+		if (s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?')
+			f++;
+		if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			f++;
+		if (f == 2)
+			s[i + 1] = s[i + 1] - 32;
+	f = 0;
+	i++;
+	}
+return (s);
+}
