ffc8c6d8bc5fd64fafb47a4ef3663819f124b95d
Andmontc <oscarandresmontes@gmail.com>
2020-02-24 16:27:57 +0000

    code that sets a buffer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,16 @@
 #include "holberton.h"
 /**
-*
-*
-*
-*
+* _memset- Function ta¡hat sets a buffer
+* @s: array
+* @b: array to set
+* @n: number of iterations
+* Return: array
 */
-char *_memset(char*s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while ( i < n )
+	while (i < n)
 	{
 		s[i] = b;
 	i++;
