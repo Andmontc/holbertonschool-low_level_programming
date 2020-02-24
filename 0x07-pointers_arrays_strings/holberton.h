f6672aed892ba7886fa61eba8400f947f0cf5362
Andmontc <oscarandresmontes@gmail.com>
2020-02-24 16:54:22 +0000

    prototypes files

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,15 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+
+#endif
