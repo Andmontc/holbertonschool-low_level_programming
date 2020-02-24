06af467f503f3de848553bd48e0628a4b83e0660
Andmontc <oscarandresmontes@gmail.com>
2020-02-24 23:01:28 +0000

    code that searches for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,13 +4,13 @@
 * @accept: second string
 * Return: string with occurences
 */
-char *_strpbrk(char *s, char*accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
 	for (i = 0; s[i]; i++)
 	{
-		for ( j = 0; accept[j]; j++)
+		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
