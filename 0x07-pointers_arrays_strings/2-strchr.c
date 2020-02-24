177a6c96d16119cab092bf97c773088e0343eaa1
Andmontc <oscarandresmontes@gmail.com>
2020-02-24 18:46:47 +0000

    code that locate a char

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _strchr-function that locates a character in a string
+* @s: first parameter
+* @c: second parameter
+* Return: string
+*/
+char  *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c || s[i] == '\0')
+		{
+			return (s + i);
+		}
+	}
+
+return (s);
+}
+
+
