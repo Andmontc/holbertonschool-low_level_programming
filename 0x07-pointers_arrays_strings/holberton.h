23411878151a2b220f5fe619e5823bc42b0be56d
Andmontc <oscarandresmontes@gmail.com>
2020-02-25 01:08:28 +0000

    prototype files

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -14,5 +14,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char*accept);
+void set_string(char **s, char *to);
 
 #endif
