f855ae2ba7d68fc04b92e38e29e2d71866ce574d
Andmontc <oscarandresmontes@gmail.com>
2020-02-25 01:57:00 +0000

    code for set a pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+* set_string - function that set the value of a pointer to a char
+* @s: first parameter
+* @to: second parameter
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
