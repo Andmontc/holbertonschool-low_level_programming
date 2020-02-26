cc356a361fcea7cb99c8325a7fe6cdf29e597136
Andmontc <oscarandresmontes@gmail.com>
2020-02-26 15:39:17 +0000

    prototype files

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,13 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+#endif
