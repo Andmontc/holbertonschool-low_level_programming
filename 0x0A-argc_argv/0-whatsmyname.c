7519419f617927d0f6aae78dcb2454118dc406f9
Andmontc <oscarandresmontes@gmail.com>
2020-02-28 23:56:29 +0000

    code that prints the program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*  main - entry function
+*  @argc: contador
+*  @argv: vector
+*  Return: 0 sucess
+*/
+int main(int argc, char *argv[])
+{
+	argc++;
+	printf("%s\n", argv[0]);
+	return (0);
+}
