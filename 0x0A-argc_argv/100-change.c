b5187434dec10e0d0add5abcf4dbd3793e33888f
Andmontc <oscarandresmontes@gmail.com>
2020-03-01 23:23:34 +0000

    code that give change

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry function
+ * @argc: counter
+ * @argv: vector
+ * Return: 1 error, 0 sucess
+ */
+int main(int argc, char *argv[])
+{
+	int i, cents, cambio = 0;
+	int coins[5] = {25, 10, 5, 2, 1};
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+
+	for (i = 0; i < 5; i++)
+	{
+		if (cents < 0)
+	{
+		putchar('0');
+		putchar('\n');
+		break;
+	}
+		if (cents / coins[i])
+		{
+			cambio += cents / coins[i];
+			cents %= coins[i];
+		}
+	}
+printf("%d\n", cambio);
+return (0);
+}
