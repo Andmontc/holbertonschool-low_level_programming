60d992338f2b25a92de28bee58897b0f7d574191
Andmontc <oscarandresmontes@gmail.com>
2020-03-03 18:43:08 +0000

    code that prints an array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - function that prints an array
+ * @size: size of the array
+ * @c: character
+ * Return: 0 sucess, null fail
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	s = malloc(size * sizeof(char));
+
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+	}
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	s[i] = '\0';
+	return (s);
+}
