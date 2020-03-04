2eeb9fe330525bdcb349ae6794f09ec2bee61294
Andmontc <oscarandresmontes@gmail.com>
2020-03-04 05:19:15 +0000

    code that print a 2D array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	matriz = malloc(sizeof(int) * height);
+	matriz = malloc(sizeof(int *) * height);
 	if (matriz == 0)
 	{
 		return (NULL);
