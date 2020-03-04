a81ed514b59a6d50840923aec4f5c1d789cf6b1c
Andmontc <oscarandresmontes@gmail.com>
2020-03-04 05:15:35 +0000

    code that print a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * alloc_grid - function that allocates a matrix
+ * @width: ancho
+ * @height: alto
+ * Return: matriz
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matriz;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	matriz = malloc(sizeof(int) * height);
+	if (matriz == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		matriz[i] = malloc(sizeof(int) * width);
+		if (matriz[i] == 0)
+		{
+			for (; i <= 0; i--)
+			{
+				free(matriz[i]);
+				free(matriz);
+				return (NULL);
+			}
+		}
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			matriz[i][j] = 0;
+		}
+	}
+	return (matriz);
+}
