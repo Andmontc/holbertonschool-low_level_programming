89a12d3aa4e56deea877dc0f81519f380bac4154
Andmontc <oscarandresmontes@gmail.com>
2020-03-05 19:52:33 +0000

    code that sets a memory yin an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+* _memset- Function that sets a buffer
+* @s: array
+* @b: array to set
+* @n: number of iterations
+* Return: array
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+	i++;
+	}
+return (s);
+}
+/**
+ * _calloc - function that sets a buffer
+ * @nmemb: array
+ * @size: size of the elements
+ * Return: array with the memory set
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+	if (p == 0)
+	{
+		return (NULL);
+	}
+	p = _memset(p, 0, size);
+	return (p);
+}
