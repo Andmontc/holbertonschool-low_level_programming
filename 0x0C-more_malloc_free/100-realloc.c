f6b0e9fd8e900a8f9ecbce68a85e00d12846614e
Andmontc <oscarandresmontes@gmail.com>
2020-03-05 22:46:32 +0000

    code that do realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,8 +35,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		ptr = malloc(new_size);
-		return (ptr);
+		free(ptr);
+		p = malloc(new_size);
+		return (p);
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
