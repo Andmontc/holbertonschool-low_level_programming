46133c5fbb16212a6902900ec375300a8ccb330b
Andmontc <oscarandresmontes@gmail.com>
2020-03-09 21:48:10 +0000

    prototype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,10 +6,13 @@
  * @age: age of the dog
  * @owner: owner of the dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
