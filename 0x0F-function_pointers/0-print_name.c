409434f4f3aac931fb7111e614186f8841132b1d
Andmontc <oscarandresmontes@gmail.com>
2020-03-11 18:16:40 +0000

    code that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
  * print_name - entry function
  * @name: arg name
  * @f: function as argument
- *
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name)
+	if (name && f)
 	{
 		f(name);
 	}
