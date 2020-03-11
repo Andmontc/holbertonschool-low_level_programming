5d63238dba7bf432053978f3d5b40e2d0df44710
Andmontc <oscarandresmontes@gmail.com>
2020-03-11 17:28:31 +0000

    code that compare functions

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * int_index - entry function
+ * @array: array of integers
+ * @size: size of the array
+ * @cmp: function pointer
+ * Return: sucess 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+	return (-1);
+}
