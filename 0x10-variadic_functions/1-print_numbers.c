dafd3e4733aae5137481d8bf7c371794e89178ed
Andmontc <oscarandresmontes@gmail.com>
2020-03-13 15:40:02 +0000

    print any amount of numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - function that prints any amount of numbers
+ * @separator: separator of arguments
+ * @n: number of arguments
+ * Return: print
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list vaprint;
+
+	va_start(vaprint, n);
+
+	for (i = 0; i < n && separator != NULL; i++)
+	{
+		printf("%d", va_arg(vaprint, int));
+		if (i < (n - 1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(vaprint);
+}
