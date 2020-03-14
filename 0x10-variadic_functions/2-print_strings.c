036ce27163c3cc86cdeb6f4a506155698a4858af
Andmontc <oscarandresmontes@gmail.com>
2020-03-14 17:11:58 +0000

    fix a validation

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		p = va_arg(vastring, char *);
 		if (p == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{
