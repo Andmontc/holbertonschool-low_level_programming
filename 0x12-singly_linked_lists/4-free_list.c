25b22db6810f6ac3dda9be7371b07ee2ea614d5f
Andmontc <oscarandresmontes@gmail.com>
2020-03-19 23:33:08 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *free_list - function that frees the list
+ * @head: list
+ */
+void free_list(list_t *head);
+{
+	int i;
+	list_t *ptr;
+	list_t *pos;
+
+	ptr = head;
+
+	for (i = 0; ptr != NULL; i++)
+	{
+		pos = ptr->next;
+		free(ptr->str);
+		free(ptr);
+		ptr = pos;
+	}
+}
