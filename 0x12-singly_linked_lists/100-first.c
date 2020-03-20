e897fd4af10978a2e17deb012b940cbf9f934354
Andmontc <oscarandresmontes@gmail.com>
2020-03-20 03:18:48 +0000

    function before main executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * mystartupfun - function before main
+ */
+void mystartupfun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
