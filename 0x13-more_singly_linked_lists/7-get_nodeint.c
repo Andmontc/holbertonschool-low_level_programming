4e987f99e0e5293ed6378acb2364db2440774d4e
Andmontc <oscarandresmontes@gmail.com>
2020-03-23 20:46:24 +0000

    code that prints the index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
+ * get_nodeint_at_index -  function that prints an index
+ * @index: number to print
+ * @head: head of the list
+ * Return: index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
