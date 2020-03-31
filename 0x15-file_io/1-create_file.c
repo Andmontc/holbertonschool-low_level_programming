0694f36333162b1e47c58a2c610ddcdbec33ca35
Andmontc <oscarandresmontes@gmail.com>
2020-03-31 20:59:36 +0000

    code that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,11 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	while (text_content[len] != '\0')
-		len++;
+	if (*text_content)
+	{
+		while (text_content[len] != '\0')
+			len++;
+	}
 
 	w = write(fd, text_content, len);
 
