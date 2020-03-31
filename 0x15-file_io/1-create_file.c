af4e016ff67eb3cf4c5bf47634a8b1dc5e455882
Andmontc <oscarandresmontes@gmail.com>
2020-03-31 20:27:07 +0000

    code that create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * create_file - function that creates a file
+ * @filename: Name of the file
+ * @text_content: Content of the file
+ * Return: 1 sucess, -1 fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	while (*text_content)
+		len++;
+
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
