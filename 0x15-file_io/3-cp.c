854fd77471f94d8ed4db3ccc527e6f8e657f3dcb
Andmontc <oscarandresmontes@gmail.com>
2020-04-01 01:13:01 +0000

    code that copies a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,19 +14,23 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	filefrom = open(argv[1], O_RDONLY);
+	if (filefrom == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
+
 	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fileto == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 
 	while (r != 0)
 	{
 	r = read(filefrom, buffer, 1024);
-	w = write(fileto, buffer, r);
-
-	if (filefrom == -1 || r == -1)
+	if (r == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 
-	if (fileto == -1 || w == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99); }
-
+	w = write(fileto, buffer, r);
+	if (w == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+	}
 	cff = close(filefrom);
 	if (cff == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d/n", filefrom), exit(100);
