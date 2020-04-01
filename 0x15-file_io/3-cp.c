544649b0b1290ea0021c6735724cb45b92b60390
Andmontc <oscarandresmontes@gmail.com>
2020-04-01 00:34:17 +0000

    code that copies a file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,7 +15,8 @@ int main(int argc, char *argv[])
 
 	filefrom = open(argv[1], O_RDONLY);
 	fileto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
+	while (r)
+	{
 	r = read(filefrom, buffer, 1024);
 	w = write(fileto, buffer, r);
 
@@ -23,7 +24,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]), exit(98);
 
 	if (fileto == -1 || w == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99); }
 
 	cff = close(filefrom);
 	if (cff == -1)
