84df0c368d71757bf359362cd3fb63fea035fc6e
Andmontc <oscarandresmontes@gmail.com>
2020-05-01 21:04:06 +0000

    print elemets of double linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - function that prints elements of a double linked list
+ * @h: double linked list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	if (!h)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%d \n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
