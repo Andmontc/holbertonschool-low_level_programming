a70f5c566b333e973c367b80afa62030c649cde4
Andmontc <oscarandresmontes@gmail.com>
2020-05-02 14:20:52 +0000

    add a node at the head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,9 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		free(new);
 		return (NULL);
 	}
+	
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
+	new->next = *head;
+	
 	if (*head != NULL)
 		(*head)->prev = new;
 
