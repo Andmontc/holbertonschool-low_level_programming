5bf73244d870b331f3efcc2526c9d12facdf43f2
Andmontc <oscarandresmontes@gmail.com>
2020-05-03 03:54:58 +0000

    delete node at an index given

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	for (i = 0; i < index; i++)
+	for (i = 0; i <= index; i++)
 	{
 		if (node == NULL)
 			return (-1);
