16458d11097d320c6c33451294acdc03070c00fa
Andmontc <oscarandresmontes@gmail.com>
2020-08-26 22:49:05 +0000

    Header and prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,11 @@
 #define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_help(int *array, int start, int end, int value);
+int jump_search(int *array, size_t size, int value);
+int jump_steps(int *array, int start, int end, int value);
 #endif
